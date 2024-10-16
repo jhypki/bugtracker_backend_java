7326c4d9283bd3cb77299871c960e5f7c752597b
Jakub Hypki <kubahypki7@gmail.com>
2024-10-16 22:41:33 +0200

    initial commit

---
+++ src/main/java/com/example/bugtracker_backend/BugtrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bugtracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugtrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugtrackerBackendApplication.class, args);
+    }
+
+}
